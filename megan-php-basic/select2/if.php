70224d9494fe756886ed4f3f9bbee66f756142c5
meganli <lovecezanne@gmail.com>
2025-06-09 16:05:00 +0800

    if判斷式練習

---
+++ megan-php-basic/select2/if.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<h1>第一種寫法</h1>
+<?php
+// 宣告變數
+$score=50;
+
+// 先顯示 echo結果：移到判斷式前面，不影響結果
+echo "你的成績：".$score."分";
+// echo "你的成績：$score 分";
+echo "<br>";
+echo "判定結果：";
+
+// 執行判斷式 if..else
+if($score >= 60){
+    echo'及格';
+}else{
+    echo'不及格';
+}
+
+?>
+
+<h1>第二種寫法：echo html元素+變數+字串</h1>
+<?php
+$score=50;
+if($score>=60){    
+    // echo "<span style='color:green'>及格</span>";
+    echo "<h1 style='color:green'>及格</h1>";
+}else{
+    echo "<h3 style='color:red'>不及格</h3>";
+}
+
+
+?>
+
+</body>
+</html>
