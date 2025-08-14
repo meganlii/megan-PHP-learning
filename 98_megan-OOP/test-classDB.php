ff5930bd8e49308e55de4bf99f0c473f24d60a30
meganli <lovecezanne@gmail.com>
2025-08-14 09:08:28 +0800

    8/14更新

--- 98_megan-OOP/test-classDB.php
+++ 98_megan-OOP/test-classDB.php
@@ -1,43 +1,28 @@
 <?php
+// 紀錄會談 使用者紀錄
 session_start();
 
-date_default_timezone_set("Asia/Taipei");
+// 括號內變數忘記加雙引號
+// date_default_timezone_set(Asia/Taipei);
+date_default_timezone_set('Asia/Taipei');
 
+// 小大括號之間 記得留空格
 function dd($array) {
   echo "<pre>";
   print_r($array);
   echo "</pre>";
+}
+
+// 變數不用加引號 會變成字串
+function to($url) {
+  header('location:' . $url);
+}
+
+function q($sql) {
+  $dsn = 'mysqlhost:localhot;dbname=db02;charset:utf8';
+  $pdo = 
+  return 
 
 }
 
-function q($sql){
-  $dsn = 'mysql:host=localhost;dbname=db02;chartset=utf8';
-  $pdo = new PDO($dsn, 'root', '');
-  return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-}
-
-function to($url){
-  header("location:" . $url);
-}
-
-class DB{
-  private $dsn = 'mysql:host=localhost;dbname=db02;chartset=utf8';
-  private $pdo;
-  private $table;
-
-  function __construct($table)
-  {
-    $this->table = $table;
-    $this->pdo = new PDO($this->dsn, 'root', '');
-  }
-
-  
-
-
-
-
-
-
-
-
-}
+?>
