f621685e363f9d80aeb4ab17ea6818f495cfa090
meganli <lovecezanne@gmail.com>
2025-08-14 16:28:18 +0800

    修改註解格式

--- 98_megan-OOP/test-classDB.php
+++ 98_megan-OOP/test-classDB.php
@@ -8,14 +8,15 @@ date_default_timezone_set('Asia/Taipei');
 
 // 小大括號之間 記得留空格
 function dd($array) {
-  echo "<pre>";
+  echo '<pre>';
   print_r($array);
-  echo "</pre>";
+  echo '</pre>';
 }
 
 // 變數不用加引號 會變成字串
 function to($url) {
-  header('location:' . $url);
+  // header('location:' . $url); 
+  header("location: $url");  // 雙引號內 直接以空格區分不同字串或變數
 }
 
 function q($sql) {
